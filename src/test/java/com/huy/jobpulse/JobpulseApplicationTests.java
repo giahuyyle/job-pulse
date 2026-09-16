7b27c96f3b19dd9331beeb7751aa3a232ad7e536
Gia Huy Le <huyyyylegia@gmail.com>
2026-09-16 14:28:34 +0800

    chore: init spring project

---
+++ src/test/java/com/huy/jobpulse/JobpulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huy.jobpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobpulseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
